bebd65578fca6c1caf56da013608151e8aa96e10
rakibulislamrohan <rakibulislamrohanm@gmail.com>
2023-08-15 13:42:40 +0600

    my first code

---
+++ src/Component/Nevagation-bar/Nevbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nevbar.css'
+
+const Nevbar = () => {
+    return (
+        <div className='nev'>
+               <h2>This is Nevbar</h2>
+       
+        </div>
+            
+    );
+};
+
+export default Nevbar;
