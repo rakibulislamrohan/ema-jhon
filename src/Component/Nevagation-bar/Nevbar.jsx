d6e60e6e39a5cb40d43c60d7dbc92fc89bd6fc3f
rakibulislamrohan <rakibulislamrohanm@gmail.com>
2023-08-15 20:43:35 +0600

    nevagation-bar was completed

--- src/Component/Nevagation-bar/Nevbar.jsx
+++ src/Component/Nevagation-bar/Nevbar.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Nevbar.css' 
+import logo from '../../images/Logo.svg'
+
 
 const Nevbar = () => {
     return (
-     <nav>
-        <div>
-            <img src="" alt="" />
-        </div>
-        <div>
+     <nav className='nevbar'>
 
+        <div className='logo'>
+            <img src={logo}/>
+        </div>
+
+        <div className='button'>
+              <a className='ab1' href="">Order</a>
+              <a className='ab2' href="">Order Review</a>
+              <a className='ab3' href="">Manage inventroy</a>
+              <a className='ab4' href="">Login</a>
         </div>
      </nav>
             
